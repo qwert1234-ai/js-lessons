2702be44f5b378ff3d961522adc073911d59089a
tueva041107@gmail.com <tueva041107@gmail.com>
2021-04-04 12:54:22 +0600

    Add data

---
+++ random_digits.js
@@ -0,0 +1,19 @@
+    function randomDigits() {
+		var digits = new Array();
+			
+		for(i = 0; i < 4; i++) 
+		{
+			var correctDigit = false;
+		    while (correctDigit == false)
+			{
+			    var rnd = Math.random() * 9;
+			    var rounded = Math.floor(rnd);
+			    if (!digits.includes(rounded))
+				{
+				    digits.push(rounded);
+					correctDigit = true;
+				}
+			}
+		}
+		return digits;	
+	}
